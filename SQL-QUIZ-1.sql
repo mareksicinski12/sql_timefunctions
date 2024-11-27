d867745158e2facca552d2ff64696c87607037f8
Marek Sicinski <119501979+mareksicinski12@users.noreply.github.com>
2024-11-27 22:39:59 +0100

    Update SQL-QUIZ-1.sql

--- SQL-QUIZ-1.sql
+++ SQL-QUIZ-1.sql
@@ -1,52 +1,31 @@
---CREATE OR REPLACE VIEW top_posts AS 
-	WITH RankedPosts AS (
-		SELECT 
-			q.Id AS PostId,
-			q.Title AS PostTitle,
-			q.LasActivityDate,
-			q.Tags,
-			q.Score,
-			RANK() OVER (PARTITION BY DATE(q.LasActivityDate) ORDER BY q.Score DESC) AS PostRankPerDay
-		FROM
-			posts q
-		WHERE
-			q.PostTypeId = 1
-	),
-	TopPosts AS (
-		SELECT 
-			rp.PostId,
-			rp.PostTitle,
-			rp.LasActivityDate,
-			rp.Tags,
-			rp.PostRankPerDay,
-			ARRAY(
-				SELECT 
-					a.Body
-				FROM
-					posts a
-				WHERE  
-					rp.PostId = a.ParentId AND a.PostTypeId = 2 
-				ORDER BY 
-					a.Score DESC
-				LIMIT 3
-					
-			) AS TopAnswers
-		
-		FROM 
-			RankedPosts rp
-		WHERE 
-			rp.PostRankPerDay <= 10
-	)
+WITH UserAverageScore AS (
+	SELECT
+		u.Id, 
+		u.DisplayName AS Nickname, 
+		AVG(c.Score) AS AverageScore
+	FROM 
+		users u 
+	LEFT OUTER JOIN 
+		comments c ON u.Id = c.UserId
+	GROUP BY 
+		u.Id, u.DisplayName
+),
+GlobalAverageScore AS (
 	SELECT 
-		PostId,
-    	PostTitle,
-    	LasActivityDate,
-    	Tags,
-		PostRankPerDay,
-    	TopAnswers
-	FROM
-		TopPosts
-	WHERE 
-		PostRankPerDay = 2
+		AVG(AverageScore) AS GlobalAverage 
+	FROM	
+		UserAverageScore
+)
 
-
+SELECT 	
+	u.Id,
+	u.Nickname,
+	u.AverageScore,
+	CASE 
+		WHEN u.AverageScore >= (SELECT GlobalAverage FROM GlobalAverageScore) THEN 'Good Poster'
+		ELSE 'Troll'
+	END AS Marking 
+FROM 
+	UserAverageScore u
+WHERE 
+	u.Id = 12
